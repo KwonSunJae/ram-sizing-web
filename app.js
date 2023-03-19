f7427edd84ac071bb50c8e18a5a4640707dc84bb
KwonSunJae <ksun4131@gmail.com>
2023-03-20 00:20:29 +0900

    fix port

--- app.js
+++ app.js
@@ -181,7 +181,7 @@ server.use((req, res) => {
   res.sendFile(__dirname + "/404.html");
 });
 
-server.listen(80, (err) => {
+server.listen(3000, (err) => {
   if (err) return console.log(err);
   console.log("The server is listening on port 3000");
 });
